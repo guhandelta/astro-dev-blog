88f83d619d637633a7314a77c3d8e6ed8d0a19be
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2023-12-30 16:35:00 -0500

    Added a 404 page

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+
+
+export const SITE_TITLE = 'Dev Blog';
+export const SITE_DESCRIPTION = 'Articles and Stories for Tech enthusiasts ';
+
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
